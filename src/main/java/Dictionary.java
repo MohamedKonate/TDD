16687eabe045985744a6f332f4005e495de42d4e
mohamedkonate <mohamed.konate@Live.fr>
2022-09-19 11:37:20 +0200

    Td

---
+++ src/main/java/Dictionary.java
@@ -0,0 +1,13 @@
+public class Dictionary {
+    private String name;
+    public Dictionary(String name) {
+        this.name = name;
+    }
+    public Object getName() {
+        return this.name;
+    }
+    public boolean isEmpty() {
+        return true;
+    }
+
+}
